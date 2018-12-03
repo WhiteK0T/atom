92dda4c9e33de05e9d6328ffac402ab93b229c46
SAM <nety@nety.ru>
2018-12-03 23:06:19 +0400

    Fixed all bugs Geometry + Tests

--- lecture02/src/test/java/ru/atom/geometry/PointPointCollisionTest.java
+++ lecture02/src/test/java/ru/atom/geometry/PointPointCollisionTest.java
@@ -3,49 +3,49 @@ package ru.atom.geometry;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertFalse;
+import static org.hamcrest.CoreMatchers.is;
+//import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThat;
 
-@Ignore
 public class PointPointCollisionTest {
     @Test
     public void pointSelfCollide() {
         Collider point = Geometry.createPoint(100, 100);
-        assertTrue(point.isColliding(point));
+        assertThat(point.isColliding(point), is(true));
     }
 
     @Test
     public void pointsEquals() {
         Collider point1 = Geometry.createPoint(100, 100);
         Collider point2 = Geometry.createPoint(100, 100);
-        assertTrue(point1.equals(point2));
+        assertThat(point1.equals(point2), is(true));
     }
 
     @Test
     public void equalPointsCollide() {
         Collider point1 = Geometry.createPoint(100, 100);
         Collider point2 = Geometry.createPoint(100, 100);
-        assertTrue(point1.isColliding(point2));
+        assertThat(point1.isColliding(point2), is(true));
     }
 
     @Test
     public void pointsNotCollide1() {
         Collider point1 = Geometry.createPoint(200, 100);
         Collider point2 = Geometry.createPoint(100, 100);
-        assertFalse(point1.isColliding(point2));
+        assertThat(point1.isColliding(point2), is(false));
     }
 
     @Test
     public void pointsNotCollide2() {
         Collider point1 = Geometry.createPoint(100, 100);
         Collider point2 = Geometry.createPoint(200, 100);
-        assertFalse(point1.isColliding(point2));
+        assertThat(point1.isColliding(point2), is(false));
     }
 
     @Test
     public void pointsNotCollide3() {
         Collider point1 = Geometry.createPoint(100, 100);
         Collider point2 = Geometry.createPoint(200, 200);
-        assertFalse(point1.isColliding(point2));
+        assertThat(point1.isColliding(point2), is(false));
     }
 }
